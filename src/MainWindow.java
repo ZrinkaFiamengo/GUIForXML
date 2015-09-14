5c41ae1f08b04ad9ba3a053c83fbbdfd48aead02
zrinka.fiamengo <zfiame00@fesb.hr>
2015-09-14 13:11:10 +0200

    File opening

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -7,7 +7,6 @@ import org.eclipse.swt.widgets.Button;
 
 import java.io.File;
 
-
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
@@ -66,92 +65,108 @@ public class MainWindow {
 	protected void createContents() {
 		shell = new Shell();
 		shell.setText("XML Settings");
-		shell.setSize(280, 900);
+		shell.setSize(270, 100);
 
-		try {
-			for(int i=0; i<XMLData.getInstance().getLength(); i++)
-			{
-				Node node = XMLData.getInstance().getItem(i);
-
-				if (node.getNodeType() == Node.ELEMENT_NODE)
-				{
-					Element elem =(Element)node;
-
-					groups[i] = new Group(shell, SWT.NONE);
-					int place = 10 + numberOfBlocks*55;
-					groups[i].setBounds(10, place, 230, 90);
-					groups[i].setText("Configuration " + i);
-					
-					
-					NodeList children = node.getChildNodes();
-					int num = 0;
-					for (int j=0; j<children.getLength(); j++)
+		Button openFile = new Button(shell, SWT.PUSH);
+		openFile.setBounds(20, 20, 200, 25);
+		openFile.setText("Choose XML file");
+		
+		openFile.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseDown(MouseEvent e) {
+				Button openFileButton = (Button) e.getSource();
+				openFileButton.setVisible(false);
+				try {
+					for(int i=0; i<XMLData.getInstance().getLength(); i++)
 					{
-						Node child = children.item(j);
-						if (!child.getNodeName().contains("#text")){
-							MakeBlock(i, num*30, elem, child.getNodeName());
-							numberOfBlocks++;
-							groups[i].setBounds(10, place, 230, 90 + num*30);
-							num++;
+						Node node = XMLData.getInstance().getItem(i);
+
+						if (node.getNodeType() == Node.ELEMENT_NODE)
+						{
+							Element elem =(Element)node;
+
+							groups[i] = new Group(shell, SWT.NONE);
+							groups[i].setText(node.getNodeName() +" " + i);
+							
+
+							NodeList children = node.getChildNodes();
+							int num = 0;
+							for (int j=0; j<children.getLength(); j++)
+							{
+								Node child = children.item(j);
+								if (!child.getNodeName().contains("#text")){
+									MakeBlock(i, num*30, elem, child.getNodeName());
+									numberOfBlocks++;
+									num++;
+								}
+							}
+							
+							if(i==0)
+							{
+								groups[i].setBounds(10, 10, 230, num*30 + 60);
+								shell.setSize(270, num*30 + 60);
+							}
+							else
+							{
+								groups[i].setBounds(10, groups[i-1].getLocation().y + groups[i-1].getBounds().height +20 , 230, num*30 + 60);
+								shell.setSize(270, shell.getSize().y + num*30 + 100);
+							}
+
+							buttons[i] = new Button(groups[i], SWT.PUSH);
+							buttons[i].setBounds(10, ++num*30, 200, 25);
+							buttons[i].setText("Send");
+							buttons[i].addMouseListener(new MouseAdapter() {
+								@Override
+								public void mouseDown(MouseEvent e) {
+
+									int currentGroup = 0;
+									Button b = (Button)e.getSource();
+									for(int i=0; i <XMLData.getInstance().getLength(); i++)
+										if(buttons[i] == b)
+											currentGroup = i;
+
+									Node node = XMLData.getInstance().getItem(currentGroup);
+									
+									NodeList children = node.getChildNodes();
+									int num = 0;
+									for (int j=0; j<children.getLength(); j++)
+									{
+										Node child = children.item(j);
+										if (!child.getNodeName().contains("#text"))
+										{
+											child.setTextContent(texts[currentGroup*3+num].getText());
+											num++;
+										}
+									}
+									
+									TransformerFactory transformerFactory = TransformerFactory.newInstance();
+									Transformer transformer = null;
+									try {
+										transformer = transformerFactory.newTransformer();
+									} catch (TransformerConfigurationException e1) {
+										// TODO Auto-generated catch block
+										e1.printStackTrace();
+									}
+									DOMSource source = new DOMSource(XMLData.getInstance().getDocument());
+									StreamResult result = new StreamResult(new File("C:\\Users\\ezrifia\\Desktop\\configuration.xml"));
+									try {
+										transformer.transform(source, result);
+									} catch (TransformerException e1) {
+										// TODO Auto-generated catch block
+										e1.printStackTrace();
+									}
+								}
+							});
 						}
 					}
-
-					buttons[i] = new Button(groups[i], SWT.PUSH);
-					buttons[i].setBounds(10, ++num*30, 200, 25);
-					buttons[i].setText("Send");
-					buttons[i].addMouseListener(new MouseAdapter() {
-						@Override
-						public void mouseDown(MouseEvent e) {
-							
-							int currentGroup = 0;
-							Button b = (Button)e.getSource();
-							for(int i=0; i <XMLData.getInstance().getLength(); i++)
-								if(buttons[i] == b)
-									currentGroup = i;
-							
-							Node node = XMLData.getInstance().getItem(currentGroup);
-							
-							Element elem =(Element)node;
-							
-							Node [] nodes = new Node [10];
-							
-							nodes[0] = elem.getElementsByTagName("host").item(0);
-							nodes[0].setTextContent(texts[currentGroup*3].getText());
-							
-							nodes[1] = elem.getElementsByTagName("username").item(0);
-							nodes[1].setTextContent(texts[currentGroup*3+1].getText());
-							
-							nodes[2] = elem.getElementsByTagName("password").item(0);
-							nodes[2].setTextContent(texts[currentGroup*3+2].getText());
-							
-							TransformerFactory transformerFactory = TransformerFactory.newInstance();
-							Transformer transformer = null;
-							try {
-								transformer = transformerFactory.newTransformer();
-							} catch (TransformerConfigurationException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-							DOMSource source = new DOMSource(XMLData.getInstance().getDocument());
-							StreamResult result = new StreamResult(new File("C:\\Users\\ezrifia\\Desktop\\configuration.xml"));
-							try {
-								transformer.transform(source, result);
-							} catch (TransformerException e1) {
-								// TODO Auto-generated catch block
-								e1.printStackTrace();
-							}
-						}
-					});
+				}
+				catch (Exception exception)
+				{
+					System.out.println("Erorr");
+					exception.printStackTrace();
 				}
 			}
-		}
-		catch (Exception e)
-		{
-			System.out.println("Erorr");
-			e.printStackTrace();
-		}
-		
-		XMLData.getInstance().getNodeNames();
+		});
 	}
 
 	private void MakeBlock(int i, int move, Element elem, String labelText)
--- src/XMLData.java
+++ src/XMLData.java
@@ -1,5 +1,8 @@
+import java.io.File;
 import java.io.IOException;
 
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -8,7 +11,6 @@ import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 import org.w3c.dom.Node;
-import org.w3c.dom.NamedNodeMap;
 
 public class XMLData {
 
@@ -20,29 +22,39 @@ public class XMLData {
 	
 	public static XMLData getInstance() {
 		if(instance == null) {
-			instance = new XMLData();
-			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder builder = null;
-			try {
-				builder = factory.newDocumentBuilder();
-			} catch (ParserConfigurationException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+
+			final JFileChooser chooser = new JFileChooser();
+			FileNameExtensionFilter filter = new FileNameExtensionFilter("xml files (*.xml)", "xml");
+			chooser.setFileFilter(filter);
+			int returnVal = chooser.showOpenDialog(null);
+			
+			if (returnVal == JFileChooser.APPROVE_OPTION) {
+				File file = chooser.getSelectedFile();
+
+				instance = new XMLData();
+				DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+				DocumentBuilder builder = null;
+				try {
+					builder = factory.newDocumentBuilder();
+				} catch (ParserConfigurationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				try {
+					document = builder.parse(file);
+				} catch (SAXException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				document.getDocumentElement().normalize();
+				nodeList = document.getElementsByTagName(document.getDocumentElement().getChildNodes().item(1).getNodeName());
 			}
-			try {
-				document = builder.parse("C:\\Users\\ezrifia\\Desktop\\configuration.xml");
-			} catch (SAXException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			document.getDocumentElement().normalize();
-			nodeList = document.getElementsByTagName("Configuration");
 		}
-		return instance;
-	}
+			return instance;
+		}
 	
 	public int getLength()
 	{
@@ -57,25 +69,5 @@ public class XMLData {
 	public Document getDocument()
 	{
 		return document;
-	}
-	
-	public void getNodeNames()
-	{
-		for (int i=0; i<nodeList.getLength();i++)
-		{
-			Node node = nodeList.item(i);
-			System.out.println(node.getNodeName());
-			NodeList children = node.getChildNodes();
-			int num = 0;
-			for (int j=0; j<children.getLength(); j++)
-			{
-				Node child = children.item(j);
-				if (!child.getNodeName().contains("#text")){
-					System.out.println(num +"--->"+child.getNodeName());
-					num++;
-				}
-			}
-
-		}
 	}
 }
